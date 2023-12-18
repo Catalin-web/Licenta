09fd96f17d09fb9117e3d87766f1aa1b118573e3
Catalin Bugnar <catalinbgnr@gmail.com>
2023-12-18 21:59:12 +0200

    more fixes

---
+++ LlmModel/download_model_locally_for_dev.ps1
@@ -0,0 +1,1 @@
+Invoke-WebRequest -Uri "https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGUF/resolve/main/llama-2-7b-chat.Q2_K.gguf?download=true" -OutFile "llama-2-7b-chat.Q2_K.gguf"
--- LlmModel/run-server.ps1
+++ LlmModel/run-server.ps1
@@ -5,5 +5,6 @@ if ($containerCount -gt 0) {
     docker compose down --remove-orphans
 }
 
+#docker build --no-cache -t llama-web-api .
 docker build -t llama-web-api .
 docker run -p 5000:5000 llama-web-api
